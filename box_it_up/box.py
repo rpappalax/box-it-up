33a8a1d28dcc320df628ac99a58aec326401f342
Richard Pappalardo <rpappalax@gmail.com>
2014-07-17 21:00:25 -0700

    More refactoring and cell formatting (WIP).

--- box_it_up/box.py
+++ box_it_up/box.py
@@ -3,10 +3,6 @@
 import sys
 
 # '''Python class for formatting various kinds of table data into an ascii table.'''
-MINIMAL = 3
-SIMPLE = 0
-OUTLINE = 1
-OUTLINE_DBL = 2
 
 POS_NW = 0
 POS_N = 1
@@ -40,9 +36,11 @@ class Box(object):
     * Add 'colspan' type capability
     """
 
-    def __init__(self):
+    def __init__(self, header=True, type='OUTLINE'):
 
         self._box = ''
+        self.header = header
+        self.box_type = type
 
         # table char types
         # corners
@@ -64,6 +62,20 @@ class Box(object):
 
         self._col_orientations = []
 
+    @property
+    def box_type(self):
+        return self._box_type
+
+    @box_type.setter
+    def box_type(self, type_str):
+        if type_str == 'MINIMAL':
+            self._box_type = 3
+        elif type_str == 'SIMPLE':
+            self._box_type = 0
+        elif type_str == 'OUTLINE':
+            self._box_type = 1
+        else: # type_str == 'OUTLINE_DBL'
+            self._box_type = 2
 
     @property
     def box(self):
@@ -96,99 +108,124 @@ class Box(object):
 
     @property
     def col_orientations(self):
+        """Returns column orientations as a list - 1 per column"""
         return self._col_orientations
 
     @col_orientations.setter
     def col_orientations(self, list_orientations):
         """optional parameter: set column orientations in a list
-        default: all cols are left-oriented
+        Note: all columns left-oriented by default
         Example:
         list_orientations = [ '<', '<', '^', '>', '>' ]"""
         self._col_orientations = list_orientations
 
-    def get_header_row(self, col, type=MINIMAL):
+    def get_header_row(self, col):
+        """provides top header frame (row) as string"""
         this_row = ''
-        # data = self.get_col_formatted(col)
-        if self.is_col_first(col):
-            this_row += self.HDR_NW[type]
-        else:
-            this_row += self.HDR_N[type]
-        this_row += self.HLINE[type] * (self._max_col_lens[col] + 1)
-
-        if self.is_col_last(col):
-            this_row += self.HDR_NE[type]
-        return this_row
-
-    def get_data_row(self, col, data, type=MINIMAL):
-        this_row = ''
-        data = self.get_col_formatted(col, data)
+        data = self.get_cell_formatted(col)
         if self.is_col_first(col):
             this_row += '\n'
-            this_row += self.DATA_W[type]
+            this_row += self.HDR_NW[self.box_type]
         else:
-            this_row += self.DATA_X[type]
+            this_row += self.HDR_N[self.box_type]
+        this_row += self.HLINE[self.box_type] * (self._max_col_lens[col] + 1)
+
+        if self.is_col_last(col):
+            this_row += self.HDR_NE[self.box_type]
+        return this_row
+
+    def get_header_row_bottom(self, col):
+        """provides bottom header frame (row) as string"""
+        this_row = ''
+        data = self.get_cell_formatted(col)
+        if self.is_col_first(col):
+            this_row += '\n'
+            this_row += self.DATA_W[self.box_type]
+        else:
+            this_row += self.DATA_X[self.box_type]
+        this_row += self.HLINE[self.box_type] * (self._max_col_lens[col] + 1)
+
+        if self.is_col_last(col):
+            this_row += self.DATA_E[self.box_type]
+        return this_row
+
+    def get_data_row(self, col, data):
+        """returns data (or header text) row as string"""
+        this_row = ''
+        data = self.get_cell_formatted(col, data)
+        if self.is_col_first(col):
+            this_row += '\n'
+        this_row += self.VLINE[self.box_type]
 
         this_row += str(data)
         if self.is_col_last(col):
-            this_row += self.DATA_E[type]
+            this_row += self.VLINE[self.box_type]
         return this_row
 
-    def get_footer_row(self, col, type=MINIMAL):
+    def get_footer_row(self, col):
+        """returns footer frame row as string"""
         this_row = ''
         if self.is_col_first(col):
             this_row += '\n'
-            this_row += self.DATA_SW[type]
+            this_row += self.DATA_SW[self.box_type]
             for i in xrange(0, self._max_col_lens[col] + 1):
-                this_row += self.HLINE[type]
+                this_row += self.HLINE[self.box_type]
         else:
-            this_row += self.DATA_S[type]
+            this_row += self.DATA_S[self.box_type]
             for i in xrange(0, self._max_col_lens[col] + 1):
-                this_row += self.HLINE[type]
+                this_row += self.HLINE[self.box_type]
 
         if self.is_col_last(col):
-            this_row += self.DATA_SE[type]
+            this_row += self.DATA_SE[self.box_type]
         return this_row
 
-    def get_col_formatted(self, col, data=''):
+    def get_cell_formatted(self, col, data=''):
+        """returns formatted cell with proper spacing, orientation"""
 
         spacer_len = (self._max_col_lens[col] - len(str(data)))
         spacer = spacer_len * ' '
         if self._col_orientations[col] == '<':
             cell = ' {}{} '.format(data,spacer)
-            # cell = ' ' + data + spacer
         elif self._col_orientations[col] == '>':
-            # cell = spacer + data + ' '
             cell = ' {}{} '.format(spacer,data)
         else:
-            spacer_len = spacer_len // 2
-            print spacer_len
-            spacer, remainder = divmod(spacer_len, 2)
+            spacer_len, remainder = divmod(spacer_len, 2)
             spacer = spacer_len * ' '
-            extra = remainder * ' '
-            # cell = spacer  + data + spacer
-            cell = ' {}{}{} '.format(spacer,data,spacer,extra)
+            if remainder !=0:
+                extra = ' '
+            else:
+                extra = ''
+            cell = ' {}{}{} {}'.format(spacer,data,spacer,extra)
 
         return cell
 
-    def box_it_new(self, type=MINIMAL):
-        box = ''
+    def box_it_new(self):
         """
+        returns box formatted data as string
         To format each column we need a list containing:
         1) max str length of each column
         2) optional:  orientation of each column: <, >, ^ (default: <)
         """
+        box = ''
 
-        for row, row_list in enumerate(self._box):
+        # header
+        if self.header:
+            for col, data in enumerate(self._box):
+                box += self.get_header_row(col)
+        for col, data in enumerate(self._box[0]):
+            box += self.get_data_row(col, data)
+        for col, data in enumerate(self._box):
+            box += self.get_header_row_bottom(col)
+
+        # data
+        for row, row_list in enumerate(self._box[1:]):
             for col, data in enumerate(row_list):
-                if self.is_header(row):
-                    box += self.get_header_row(col, type)
-                else:
-                    box += self.get_data_row(col, data, type)
+                box += self.get_data_row(col, data)
+
+        # footer
         for col, data in enumerate(row_list):
-            box += self.get_footer_row(col, type)
+            box += self.get_footer_row(col)
         return box
-
-
 
     def is_tabular(self, data):
         """Checks if data is tabular (each list in list must be of same length)"""
@@ -207,58 +244,10 @@ class Box(object):
     def is_row_last(self, row_pos):
         return (self._rows - 1 == row_pos)
 
-    def box_double(self, list_data, header=False):
-        """Returns table data as double-bar formatted string table."""
-        pass
-
-    def get_box_specify(self, type = 'SIMPLE'):
-        print self.HDR_NW[type]
-        box = u'{}{}{}{}{}'.format(
-            self.HDR_NW[type],
-            self.HLINE[type],
-            self.HDR_N[type],
-            self.HLINE[type],
-            self.HDR_NW[type]
-        )
-        return box
-
-    def box_it(self, results, type=SIMPLE):
-        """takes a list of lists (key, value pairs) and wraps them in a grid
-        results = [
-            [ 'AVG TEST DURATION', '2s' ],
-            [ 'TEST RESULTS', 'PASS: 2, FAIL: 0, ERROR: 3' ],
-            [ 'XXXXXXXXXXXXXXXXXXXXXXX', 'YYYYYYYYYY' ],
-            [ 'AAA', 'BBBB' ]
-        ] """
-        spacer = '   '
-        len_spacer = len(spacer)
-        max_len_keys = max([ len(str(key)) for key, val  in results])
-        results_reversed = [(t[1], t[0]) for t in results]
-        max_len_vals = max([ len(str(val)) for val, key  in results_reversed])
-        line_k= (max_len_keys + len_spacer) * self.HLINE[type]
-        line_v= (len_spacer + max_len_vals + len_spacer) * self.HLINE[type]
-        len_cell_k = max_len_keys + len_spacer
-        # len_cell_v = max_len_vals + len_spacer
-        i =0
-        box = ''
-        for k, v in results:
-            spacer_k = (len_cell_k - len(k)) * ' '
-            box +=  u'{}{}{}'.format(k + spacer_k, self.VLINE[type], spacer + v) + '\n'
-            if len(results) > 1 and i < len(results) - 1:
-                box += u'{}{}{}'.format(line_k, self.HDR_S[type], line_v) + '\n'
-            i += 1
-        return box
-
-
-
-
 
 if __name__ == '__main__':
 
-    box = Box()
-    # print box.get_box_specify(SIMPLE)
-    # print box.get_box_specify(OUTLINE)
-    # print box.get_box_specify(OUTLINE_DBL)
+    box = Box(type='OUTLINE', header=True)
 
     # results = [
     #         [ 'AVG TEST DURATION', '2s' ],
@@ -277,15 +266,16 @@ if __name__ == '__main__':
         [ 'aaa','bbb','ccc','dddd','eeee'],
         [ 'ffff','gggg','hhh','iiiiiiiiiiiii','jjjjjj'],
         [ 'kk','lllllll','m','nnnnnn','oooooo'],
-        [ 'ppppp','qq','rrr','sssss','t'],
+        [ 'ppppp','qq','rrrr','sssss','t'],
         [ 'u','vvv','ww','xxx','yyyyyyyyyyyyyyyy']
     ]
     box.col_orientations = [ '>', '<', '^', '>', '>']
 
-    # print box.box_it(results, SIMPLE)
-    # print box.box_it(results, OUTLINE)
-    # print box.box_it(results, OUTLINE_DBL)
+    # print box.box_it(results)
+    # print box.box_it(results)
+    # print box.box_it(results)
     box.box = results
-    print box.box_it_new(OUTLINE_DBL)
-    print box.box_it_new(OUTLINE)
+    print box.box_it_new()
+    box.box_type = 'OUTLINE_DBL'
+    print box.box_it_new()
 
