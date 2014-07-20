03eef36a6cd12bae024cf8b7dd96c3082577281b
Richard Pappalardo <rpappalax@gmail.com>
2014-07-19 20:51:41 -0700

    Adding PyPI files.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "box-it-up",
+    version = "0.0.1",
+    description = "Python class for formatting various kinds of table data into an ascii table.",
+    author = "Richard Pappalardo",
+    author_email = "rpappalax@gmail.com",
+    url = "https://github.com/rpappalax/box-it-up",
+    install_requires = [],
+    packages = find_packages(),
+    keywords = ['testing', 'logging', 'reporting', 'stats'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        ]
+    )
